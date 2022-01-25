2641a852744df3365207ea694fa8941f89b8d56f
strzalkom <mateusz.strzalko@gmail.com>
2022-01-25 22:19:54 +0100

    bug fix
    bug fix1

---
+++ RESTApiWithAzureFunction/RESTApiWithAzureFunction/Models/TaskModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTApiWithAzureFunction.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+    public class CreateTaskModel
+    {
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public string Description { get; set; }
+    }
+    public class UpdateTaskModel
+    {
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
